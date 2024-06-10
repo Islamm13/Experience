80a643454e94c7f3e7a8fe0898257ece7fa3116c
Islam <islamm1993m@gmail.com>
2024-06-10 18:35:41 +0400

    Practica task

--- src/lesson9/PracticaTask.java
+++ src/lesson9/PracticaTask.java
@@ -4,37 +4,36 @@ import java.util.Scanner;
 
 public class PracticaTask {
 
-    public static void main(String[] args) {
+    public static <let> void main(String[] args) {
+
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Reqem daxil edin:");
+//
+//        int a = scanner.nextInt();
+//        int b =0;
+//
+//
+//       while (a!=0){
+//           a/=10;
+//           b++;
+//
+//       }
+//        System.out.println(b+" Reqemlidir");
+
 
 
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Reqem Daxil Edin:");
+        int num = scanner.nextInt();
 
-        long floors = scanner.nextLong();
 
-        if (floors>= 0 && floors <= 9) {
-            System.out.println("1 Ededlidir:");
+        for (int i = 1; i <= 10; i++) {
 
-        } else if (floors>=10 && floors<=99) {
-            System.out.println("2 Ededlidir:");
-
-        } else if (floors>=100 && floors<=999) {
-            System.out.println("3 Ededlidir:");
-
-        } else if (floors>=1000 && floors<=9999) {
-            System.out.println("4 Ededlidir:");
-
-        } else if (floors>=10000 && floors<=99999) {
-            System.out.println("5 Ededlidir:");
-
-        } else if (floors>=100000 && floors<=999999) {
-            System.out.println("6 Ededlidir:");
-
-        } else if (floors>=1000000 && floors<=9999999) {
-            System.out.println("7 Ededlidir:");
-
-        }else {
-            System.out.println("Cox Ededlidir:");
+            System.out.println(num + "x" + i + "=" + (i * num));
         }
+
+        System.out.println("***********************************");
 
     }
 
